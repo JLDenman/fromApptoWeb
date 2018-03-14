c311ab71a070f95262b5e7b68519e4f1d4469139
Josh <denman.joshua@yahoo.com>
2018-03-14 16:18:47 -0700

    Add project files.

---
+++ fromApptoWeb/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace fromApptoWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.googleMapsBrowser = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_street = new System.Windows.Forms.TextBox();
+            this.txt_state = new System.Windows.Forms.TextBox();
+            this.txt_city = new System.Windows.Forms.TextBox();
+            this.txt_zip = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.search_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.search_button);
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_zip);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_city);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_state);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_street);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.googleMapsBrowser);
+            this.splitContainer1.Size = new System.Drawing.Size(797, 468);
+            this.splitContainer1.SplitterDistance = 265;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // googleMapsBrowser
+            // 
+            this.googleMapsBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.googleMapsBrowser.Location = new System.Drawing.Point(0, 0);
+            this.googleMapsBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.googleMapsBrowser.Name = "googleMapsBrowser";
+            this.googleMapsBrowser.Size = new System.Drawing.Size(528, 468);
+            this.googleMapsBrowser.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Where to next?";
+            // 
+            // txt_street
+            // 
+            this.txt_street.Location = new System.Drawing.Point(141, 71);
+            this.txt_street.Name = "txt_street";
+            this.txt_street.Size = new System.Drawing.Size(100, 20);
+            this.txt_street.TabIndex = 1;
+            // 
+            // txt_state
+            // 
+            this.txt_state.Location = new System.Drawing.Point(141, 146);
+            this.txt_state.Name = "txt_state";
+            this.txt_state.Size = new System.Drawing.Size(100, 20);
+            this.txt_state.TabIndex = 2;
+            // 
+            // txt_city
+            // 
+            this.txt_city.Location = new System.Drawing.Point(141, 108);
+            this.txt_city.Name = "txt_city";
+            this.txt_city.Size = new System.Drawing.Size(100, 20);
+            this.txt_city.TabIndex = 3;
+            // 
+            // txt_zip
+            // 
+            this.txt_zip.Location = new System.Drawing.Point(141, 184);
+            this.txt_zip.Name = "txt_zip";
+            this.txt_zip.Size = new System.Drawing.Size(100, 20);
+            this.txt_zip.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Street";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "City";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "State";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Zip Code";
+            // 
+            // search_button
+            // 
+            this.search_button.Location = new System.Drawing.Point(99, 258);
+            this.search_button.Name = "search_button";
+            this.search_button.Size = new System.Drawing.Size(75, 23);
+            this.search_button.TabIndex = 9;
+            this.search_button.Text = "Search";
+            this.search_button.UseVisualStyleBackColor = true;
+            this.search_button.Click += new System.EventHandler(this.search_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 468);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button search_button;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_zip;
+        private System.Windows.Forms.TextBox txt_city;
+        private System.Windows.Forms.TextBox txt_state;
+        private System.Windows.Forms.TextBox txt_street;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser googleMapsBrowser;
+    }
+}
+
---
+++ fromApptoWeb/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fromApptoWeb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void search_button_Click(object sender, EventArgs e)
+        {
+            string street = txt_street.Text;
+            string city = txt_city.Text;
+            string state = txt_state.Text;
+            string zip = txt_zip.Text;
+            try
+            {
+                StringBuilder mapadress = new StringBuilder();
+                mapadress.Append("https://maps.google.com/maps?q=");
+
+                if (street != string.Empty)
+                {
+                    mapadress.Append(street + ",+");
+                }
+                if (city != string.Empty)
+                {
+                    mapadress.Append(city + ",+");
+                }
+                if (state != string.Empty)
+                {
+                    mapadress.Append(state + ",+");
+                }
+                if (zip != string.Empty)
+                {
+                    mapadress.Append(zip + ",+");
+                }
+
+                googleMapsBrowser.Navigate(mapadress.ToString());
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+    }
+}
